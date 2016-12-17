e71ad936aa071bf90e8d18c246ddf9692726d824
Csaba Lakatos <lacsaba@gmail.com>
2016-12-17 16:54:26 +0100

    added a basic $watch and $digest

--- src/scope.js
+++ src/scope.js
@@ -2,6 +2,24 @@
  * Created by LCsaba on 16/12/17.
  */
 'use strict';
+
+var _ = require('lodash');
 module.exports = function Scope () {
-    return {};
+    this.$$watchers = [];
+
+    Scope.prototype.$watch = function (watchFn, listenerFn) {
+        var watcher = {
+            watchFn: watchFn,
+            listenerFn: listenerFn
+        };
+        this.$$watchers.push(watcher);
+    };
+
+    Scope.prototype.$digest = function () {
+        var self = this;
+        _.forEach(this.$$watchers, function (watcher) {
+            watcher.watchFn(self);
+            watcher.listenerFn();
+        })
+    };
 };
